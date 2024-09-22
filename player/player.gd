c976df4ed398d2c1865cf1401f6ba569f37b8573
VampireSqu1d <cuauhtemoc.ayala@proton.me>
2024-09-22 16:36:05 -0700

    added obstacles to sandbox

--- player/player.gd
+++ player/player.gd
@@ -1,18 +1,30 @@
 extends CharacterBody3D
 
+@export var fall_multiplier: float = 2.0
+@export var jump_height: float = 2.0
+@onready var camera_pivot: Node3D = $CameraPivot
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED: = 5.0
+#const JUMP_VELOCITY: = 4.5
 
+var mouse_motion: = Vector2.ZERO
+var mouse_sens: = 0.001
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	handle_camera_rotation()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y >= 0:
+			velocity += get_gravity() * delta
+		else:
+			velocity += get_gravity() * delta * fall_multiplier
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = sqrt(jump_height * 2.0 * -get_gravity().y)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,3 +38,19 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_motion = -event.relative * mouse_sens
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+
+func handle_camera_rotation() -> void:
+	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clampf(camera_pivot.rotation_degrees.x, -90.0, 90.0)
+	mouse_motion = Vector2.ZERO
